dbfb39d5356b64d1678da93aea616e12387019f7
REYEMDMEN <rey.emd.mendoza@gmail.com>
2024-12-09 12:29:45 -0500

    Add files via upload

---
+++ home.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('.faq-question').forEach(button => {
+    button.addEventListener('click', () => {
+      const answer = button.nextElementSibling;
+      answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+    });
+  });
+  
